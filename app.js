23053fa008d30426e29e0e223e96c78e0ad3604c
James Prentice <jamesprenticez@gmail.com>
2021-09-24 21:01:16 +1200

    multiplayer and disconnect

--- app.js
+++ app.js
@@ -15,16 +15,41 @@ server.listen(PORT)
 console.log("Server started on port: " + PORT)
 
 // SOCKET.IO
+
+var SOCKET_LIST = {}
+
 var io = require("socket.io")(server, {});
 io.sockets.on("connection", function(socket){
+
     console.log("Socket connected")
 
-    socket.on("happy", function(data){
-        console.log("happy socket " + data.reason)
-    })
+    socket.id = Math.random();
+    socket.x = 0
+    socket.y = 0
+    socket.number = "" + Math.floor(10 * Math.random())
 
-    socket.emit("serverMsg", {
-        msg: "yeah im talking to you!",
-    })
+    SOCKET_LIST[socket.id] = socket;
 
+    socket.on("disconnect", function(){
+        delete SOCKET_LIST[socket.id]
+    })
 })
+
+// For every player connected loop through SOCKETLIST and update there position
+setInterval(function(){
+    var pack = [];
+    for(var i in SOCKET_LIST){
+            var socket = SOCKET_LIST[i]
+            socket.x++
+            socket.y++
+            pack.push({
+                x: socket.x,
+                y: socket.y,
+                number: socket.number
+            })
+        }
+    for(var i in SOCKET_LIST){
+        var socket = SOCKET_LIST[i]
+        socket.emit("newPositions", pack)
+    }
+},1000/25);
